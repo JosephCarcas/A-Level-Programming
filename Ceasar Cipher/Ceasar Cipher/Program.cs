9a508e7c2e0297937f784d8e6c4073cdfee36337
JosephCarcas <57670857+JosephCarcas@users.noreply.github.com>
2019-11-12 14:30:37 +0000

    Start of Program

---
+++ Ceasar Cipher/Ceasar Cipher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Encryption("Cheese Please!", 15);
+            Console.WriteLine(message);
+            Console.WriteLine(Decryption(message, 15));
+
+        }
+
+        static string Encryption(string plainText, int shiftKey)
+        {
+            string cipherText = "";
+
+            return cipherText;
+        }
+
+        static string Decryption(string cipherText, int shiftKey)
+        {
+            string plainText = "";
+
+            return plainText;
+        }
+    }
+}
